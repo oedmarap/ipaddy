9fce4690526601a0798b2715d1fa0ed51df618d1
Paramdeo Singh <paramdeo@paramdeo.com>
2020-01-12 22:24:24 -0400

    Added explicit content type, headers, etc.

--- serverless/cloudflare-worker.js
+++ serverless/cloudflare-worker.js
@@ -2,5 +2,12 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 async function handleRequest(request) {
-  return new Response(request.headers.get('CF-Connecting-IP'), {status: 200})
+  let response = fetch(request)
+  response.body = request.headers.get('CF-Connecting-IP')
+  response = new Response(response.body, response)
+  response.headers.set('Content-Type', 'text/plain')
+  response.headers.set('Access-Control-Allow-Origin', '*')
+  response.headers.set('Access-Control-Allow-Methods', 'GET')
+  response.headers.set('X-NFO', 'Find out more at https://use.ipaddy.net')
+  return response
 }
