c431abaef40c49cebc5c33b3885653379206a84e
Paramdeo Singh <paramdeo@paramdeo.com>
2020-06-26 08:28:00 -0400

    Edited response body for better UX

--- serverless/cloudflare-worker.js
+++ serverless/cloudflare-worker.js
@@ -1,9 +1,9 @@
-addEventListener('fetch', event => {
+ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 async function handleRequest(request) {
   let response = fetch(request)
-  response.body = request.headers.get('CF-Connecting-IP')
+  response.body = "IP Address: " + request.headers.get('CF-Connecting-IP') + "\n\nIP Country: " + request.headers.get('CF-IPCountry') + "\n\nUser Agent: " + request.headers.get('User-Agent') + "\n"
   response = new Response(response.body, response)
   response.headers.set('Content-Type', 'text/plain')
   response.headers.set('Access-Control-Allow-Origin', '*')
